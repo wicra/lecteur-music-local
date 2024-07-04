9a18eec02ac4ecbfa9064a03efa04738858171f4
Wicramachine Sergio <wicramachine@gmail.com.com>
2024-07-04 13:14:43 +0200

    Maj

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 </head>
 <body>
     <h2>Télécharger Playlist YouTube en MP3</h2>
-    <form action="download.php" method="post">
+    <form action="download.php" method="post" enctype="multipart/form-data">
         <label for="playlist_url">URL de la playlist YouTube :</label><br>
         <input type="text" id="playlist_url" name="playlist_url" required><br><br>
         
         <label for="output_dir">Répertoire de sortie :</label><br>
-        <input type="text" id="output_dir" name="output_dir" required><br><br>
+        <input type="file" id="output_dir" name="output_dir" webkitdirectory directory required><br><br>
         
         <input type="submit" value="Télécharger">
     </form>
